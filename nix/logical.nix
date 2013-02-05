88f591797b33f2bae55efaebb68876b59fe15b1a
Shea Levy <shea@shealevy.com>
2013-02-05 16:21:22 -0500

    logical charon network: Use EC2 metadata for credentials

--- nix/logical.nix
+++ nix/logical.nix
@@ -1,22 +1,5 @@
-let
-  defaultAwsKeyId = 
-    let
-      res = builtins.getEnv "EC2_ACCESS_KEY";
-    in if res == ""
-      then throw "You must either set the awsKeyId argument or define the EC2_ACCESS_KEY env var"
-      else res;
-
-  defaultAwsSecretKey = 
-    let
-      res = builtins.getEnv "EC2_SECRET_KEY";
-    in if res == ""
-      then throw "You must either set the awsSecretKey argument or define the EC2_ACCESS_KEY env var"
-      else res;
-in
 { environment ? "production"
 , googleAnalyticsId ? ""
-, awsKeyId ? defaultAwsKeyId
-, awsSecretKey ? defaultAwsSecretKey
 , jotFormId ? ""
 , shareThisId ? ""
 , maxUploadSize ? 100
@@ -27,22 +10,16 @@ in
   network.name = "mindmup";
 
   machine = { config, resources, ... }: {
-    deployment = {
-      storeKeysOnMachine = false;
-
-      keys."s3-secret.key" = awsSecretKey;
-    };
-
     require = [ ./mindmup.nix ];
 
     services.mindmup = {
       enable = true;
 
-      inherit googleAnalyticsId awsKeyId jotFormId shareThisId maxUploadSize networkTimeout;
+      inherit googleAnalyticsId jotFormId shareThisId maxUploadSize networkTimeout;
+
+      getAWSCredentialsFromEC2Metadata = true;
 
       s3BucketName = resources.s3Buckets.mindmup.name;
-
-      awsSecretKeyFile = "/run/keys/s3-secret.key";
 
       s3UploadFolder = "maps";
 
