a0c695e39e1be43cb6f3a244dce6f45f17af7155
Shea Levy <shea@shealevy.com>
2013-02-06 12:16:23 -0500

    Clean up nix expressions

--- nix/logical.nix
+++ nix/logical.nix
@@ -1,33 +1,12 @@
-{ environment ? "development"
-, googleAnalyticsId ? ""
-, jotFormId ? ""
-, shareThisId ? ""
-, maxUploadSize ? 100
-, networkTimeout ? 10000
-}:
-
 {
   network.name = "mindmup";
-
   machine = { config, resources, ... }: {
     require = [ ./mindmup.nix ];
-
     ec2.metadata = true;
-
     services.mindmup = {
       enable = true;
-
-      inherit googleAnalyticsId jotFormId shareThisId maxUploadSize networkTimeout;
-
-      getAWSCredentialsFromEC2Metadata = true;
-
       s3BucketName = resources.s3Buckets."mindmup-bucket".name;
-
-      s3UploadFolder = "maps";
-
       siteURL = "http://${config.networking.publicIPv4}/";
-
-      rackEnvironment = environment;
     };
   };
 }
--- nix/physical.nix
+++ nix/physical.nix
@@ -1,57 +1,47 @@
-{ region ? "us-east-1"
-, instanceType ? "m1.small"
-}:
+{ region ? "us-east-1" }:
 {
-  resources.s3Buckets."mindmup-bucket" = {
-    inherit region;
-
-    accessKeyId = "mindmup";
+  resources = {
+    s3Buckets."mindmup-bucket" = {
+      inherit region;
+      accessKeyId = "mindmup";
+    };
+    ec2KeyPairs."mindmup-key-pair" = {
+      inherit region;
+      accessKeyId = "mindmup";
+    };
+    iamRoles."mindmup-role" = { resources, config, ... }: {
+      accessKeyId = "mindmup";
+      policy = ''{
+        "Version":"2008-10-17",
+        "Statement": [
+          {
+            "Action": [
+              "s3:ListAllMyBuckets"
+            ],
+            "Effect": "Allow",
+            "Resource": ["arn:aws:s3:::*"]
+          },
+          {
+            "Action": [
+              "s3:*"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+              "arn:aws:s3:::${resources.s3Buckets."mindmup-bucket".name}",
+              "arn:aws:s3:::${resources.s3Buckets."mindmup-bucket".name}/*"
+            ]
+          }
+        ]
+      }'';
+    };
   };
-
-  resources.ec2KeyPairs."mindmup-key-pair" = {
-    inherit region;
-
-    accessKeyId = "mindmup";
-  };
-
-  resources.iamRoles."mindmup-role" = { resources, config, ... }: {
-    accessKeyId = "mindmup";
-
-    policy = ''{
-      "Version":"2008-10-17",
-      "Statement": [
-        {
-          "Action": [
-            "s3:ListAllMyBuckets"
-          ],
-          "Effect": "Allow",
-          "Resource": ["arn:aws:s3:::*"]
-        },
-        {
-          "Action": [
-            "s3:*"
-          ],
-          "Effect": "Allow",
-          "Resource": [
-            "arn:aws:s3:::${resources.s3Buckets."mindmup-bucket".name}",
-            "arn:aws:s3:::${resources.s3Buckets."mindmup-bucket".name}/*"
-          ]
-        }
-      ]
-    }'';
-  };
-
   machine = { resources, ... }: {
     deployment = {
       targetEnv = "ec2";
-
       ec2 = {
-        inherit region instanceType;
-
+        inherit region;
         accessKeyId = "mindmup";
-
         keyPair = resources.ec2KeyPairs."mindmup-key-pair".name;
-
         instanceProfile = resources.iamRoles."mindmup-role".name;
       };
     };
